5bb5fc3d00e9bd732cfe340d223bb45c1ea08f6e
daubong240705-art <daubong240705@gmail.com>
2026-02-05 02:15:54 +0700

    dashboard page

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -1,4 +1,4 @@
-import { Film } from "lucide-react";
+import { Film, Play, User } from "lucide-react";
 
 export default function AdminPage() {
     return (
@@ -14,7 +14,31 @@ export default function AdminPage() {
                         <div className="bg-white/20 p-3 rounded-lg">
                             <Film className="w-6 h-6" />
                         </div>
+                        <span className="text-2xl font-bold">123</span>
                     </div>
+                    <h3 className="text-sm font-medium opacity-90">Tổng số phim</h3>
+                </div>
+            </div>
+            <div className="grid-col-3 gap-6 mb-8">
+                <div className="bg-gradient-to-br from-green-600 to-green-700 rounded-lg p-6 text-white">
+                    <div className="flex item-center justify-between mb-4">
+                        <div className="bg-white/20 p-3 rounded-lg">
+                            <User className="w-6 h-6" />
+                        </div>
+                        <span className="text-2xl font-bold">123</span>
+                    </div>
+                    <h3 className="text-sm font-medium opacity-90">Tổng số người dùng</h3>
+                </div>
+            </div>
+            <div className="grid-col-3 gap-6 mb-8">
+                <div className="bg-gradient-to-br from-purple-600 to-purple-700 rounded-lg p-6 text-white">
+                    <div className="flex item-center justify-between mb-4">
+                        <div className="bg-white/20 p-3 rounded-lg">
+                            <Play className="w-6 h-6" />
+                        </div>
+                        <span className="text-2xl font-bold">123</span>
+                    </div>
+                    <h3 className="text-sm font-medium opacity-90">Tổng số luợt xem</h3>
                 </div>
             </div>
         </div>
